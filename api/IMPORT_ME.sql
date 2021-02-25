e06c56449b031f96a3cdc97870adf1bc61e06ee3
Ito Sounen <sandiramadhan1996@gmail.com>
2021-02-25 15:00:38 +0700

    fixing query

--- api/IMPORT_ME.sql
+++ api/IMPORT_ME.sql
@@ -2,17 +2,13 @@
 CREATE DATABASE IF NOT EXISTS jsnow_vlookup;
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `username` varchar(30) NOT NULL,
   `password` varchar(60) NOT NULL,
   `fullname` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-INSERT INTO `users` (`id`, `username`, `password`, `fullname`) VALUES
-(1, 'jon', 'jon123', 'Jon Snow'),
-(2, 'stark', 'stark123', 'Stark');
-
-
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+INSERT INTO `users` (`username`, `password`, `fullname`) VALUES
+('jon', 'jon123', 'Jon Snow'),
+('stark', 'stark123', 'Stark');
